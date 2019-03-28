25f36a81a01e7f3f3fd3e33aa104393787883c97
joody <joody@hubify.com.br>
2019-03-28 10:12:18 -0300

    First Commit
    Installed the API application skelleton. And yes I used the structure I normally use on my API projects

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
+
+if (process.env.NODE_ENV == 'test') {
+    require('dotenv').config({
+        path: 'tests/.env'
+    });
+} else {
+    require('dotenv').config();
+}
+
+const app = express();
+const db = require('./db');
+
+const globals = {};
+
+globals.dbInstance = db;
+globals.db = {};
+
+globals.helpers = {};
+globals.helpers.hypermedia = require('./helpers/hypermedia');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(helmet());
+
+app.use(cors({
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Credentials'],
+  optionsSuccessStatus: 200,
+}));
+
+app.use(new rateLimiter({
+  windowMs: 60*1000,
+  delayAfter: 0,
+  delayMs: 0,
+  max: 30,
+  message: 'Too many requests, please try again later.',
+  statusCode: 429,
+  headers: true,
+}));
+
+const echoRoute = require('./routes/echo')(globals);
+
+app.use(echoRoute);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use((err, req, res) => {
+        res.status(err.status || 500);
+        res.send({
+            message: err.message,
+            error: err,
+        });
+    });
+}
+
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: {},
+    });
+});
+
+app.listen(process.env.APP_PORT);
+module.exports = app;
+
+module.exports.globals = globals;
